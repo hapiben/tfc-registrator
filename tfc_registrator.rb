efe2b7ec92cb16d5a66dbb46a94def9c6d309bc8
Benedict Aluan <benedict.aluan@gmail.com>
2017-03-21 18:46:41 +1300

    Fill out TFC form

--- tfc_registrator.rb
+++ tfc_registrator.rb
@@ -1,9 +1,26 @@
 require 'rubygems'
 require 'mechanize'
 
-TFC_REGISTRATION_URL = 'https://tfc.tv/User/Register'.freeze
+#
+# TfcRegistrator
+#
+class TfcRegistrator
+  TFC_REGISTRATION_URL = 'https://tfc.tv/User/Register'.freeze
 
-site = Mechanize.new
-site.get(TFC_REGISTRATION_URL) do |page|
+  def call
+    agent = Mechanize.new
+    page = agent.get(TFC_REGISTRATION_URL)
+    form = page.forms.last
+    current_time = Time.now
+    email = "hello+tfc#{current_time.day}#{current_time.month}#{current_time.year}@taxikick.com"
 
+    form.login_email = email
+    form.login_pass = 'fist-troika-apparel'
+    form.first_name = 'Hello'
+    form.last_name = 'Taxikick'
+
+    page = agent.submit(form)
+  end
 end
+
+TfcRegistrator.new.call
