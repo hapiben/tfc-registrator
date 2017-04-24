9a72c11ee6e411dc30b0bbcacc328a0b1ae32f8a
Benedict Aluan <benedict.aluan@gmail.com>
2017-04-25 10:55:45 +1200

    Make variable values general

--- tfc_registrator.rb
+++ tfc_registrator.rb
@@ -1,9 +1,6 @@
 require 'rubygems'
 require 'mechanize'
 
-#
-# TfcRegistrator
-#
 class TfcRegistrator
   TFC_REGISTRATION_URL = 'https://tfc.tv/User/Register'.freeze
 
@@ -12,12 +9,11 @@ class TfcRegistrator
     page = agent.get(TFC_REGISTRATION_URL)
     form = page.forms.last
     current_time = Time.now
-    email = "hello+tfc#{current_time.day}#{current_time.month}#{current_time.year}@taxikick.com"
 
-    form.login_email = email
-    form.login_pass = 'fist-troika-apparel'
-    form.first_name = 'Hello'
-    form.last_name = 'Taxikick'
+    form.login_email = 'email'
+    form.login_pass = 'password'
+    form.first_name = 'John'
+    form.last_name = 'Doe'
 
     agent.submit(form)
   end
